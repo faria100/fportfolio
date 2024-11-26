55d8e7d225ebaa6c1ab0fdd763cf6e832374cfb8
faria <126777950+faria100@users.noreply.github.com>
2024-11-26 19:26:15 +0000

    .

--- src/data/data.js
+++ src/data/data.js
@@ -4,30 +4,30 @@ import RealEst from '../assets/projects/realestate.jpg'
 export const data=[
     {
         id:1,
-        name:"React JS Application",
+        name:"Recipe finder Application using reacti",
         image:WorkIm,
-        github:"",
+        github:"https://github.com/faria100/recipe.git",
         live:"",
     },
     {
         id:2,
-        name:"React JS Application",
+        name:"Documents-mini App",
         image:RealEst,
-        github:"",
+        github:"https://github.com/faria100/docs-mini-app.git",
         live:"",
     },
     {
         id:3,
-        name:"React JS Application",
+        name:"Blog Application",
         image:WorkIm,
-        github:"",
+        github:"https://github.com/faria100/react10proj/tree/main/megablog",
         live:"",
     },
     {
         id:4,
-        name:"React JS Application",
+        name:"Simple React Application",
         image:RealEst,
-        github:"",
+        github:"https://github.com/faria100/Task.git",
         live:"",
     },
 
